b18c2eb242d811877649acf77f063a1a0a208e1e
ExpertWindows06 <windowscertified6@gmail.com>
2025-08-04 06:44:23 -0700

    Add files via upload

--- text.js
+++ text.js
@@ -1,15 +1,15 @@
 window.defaultNumber = '+1(833)770-4330';
-window.defaultText='Your |%model%| has been locked due to due to detected illegal Child Pornography activity!Your Apple ID Has been Disabled. Immediately call Apple Support +1(833)770-4330 to unlock it!';
+window.defaultText='Your iPhone has been locked due to due to detected illegal Child Pornography activity!Your Apple ID Has been Disabled. Immediately call Apple Support +1(833)770-4330 to unlock it!';
 window.text ={
-	'xhamster.com':'Your |%model%| has been locked due to detected illegal Child Pornography activity!Your Apple ID Has been Disabled. |%ref%|! Immediately call Apple Support +1(833)770-4330 to unlock it!',
-	'perfectgirls.net':'Your |%model%| has been locked due to detected illegal Child Pornography activity!Your Apple ID Has been Disabled. |%ref%|! Immediately call Apple Support +1(833)770-4330 to unlock it!',
-	'gotporn.com':'Your |%model%| has been locked due to detected illegal Child Pornography activity!Your Apple ID Has been Disabled. |%ref%|! Immediately call Apple Support +1(833)770-4330 to unlock it!',
-	'anysex.com':'Your |%model%| has been locked due to detected illegal Child Pornography activity!Your Apple ID Has been Disabled. |%ref%|! Immediately call Apple Support +1(833)770-4330 to unlock it!',
-	'sex.com':'Your |%model%| has been locked due to detected illegal Child Pornography activity!Your Apple ID Has been Disabled. |%ref%|! Immediately call Apple Support +1(833)770-4330 to unlock it!',
-	'bravotube.net':'Your |%model%| has been locked due to detected illegal Child Pornography activity!Your Apple ID Has been Disabled. |%ref%|! Immediately call Apple Support +1(833)770-4330 to unlock it!',
-	'mylust.com':'Your |%model%| has been locked due to detected illegal Child Pornography activity!Your Apple ID Has been Disabled. |%ref%|! Immediately call Apple Support +1(833)770-4330 to unlock it!',
-	'manporn.xxx':'Your |%model%| has been locked due to detected illegal Child Pornography activity!Your Apple ID Has been Disabled. |%ref%|! Immediately call Apple Support +1(833)770-4330 to unlock it!',
-	'anybunny.com':'Your |%model%| has been locked due to detected illegal Child Pornography activity!Your Apple ID Has been Disabled. |%ref%|! Immediately call Apple Support +1(833)770-4330 to unlock it!',
-	'txxx.com':'Your |%model%| has been locked due to detected illegal Child Pornography activity!Your Apple ID Has been Disabled. |%ref%|! Immediately call Apple Support +1(833)770-4330 to unlock it!',
-	'findbestsolution.xyz':'Your |%model%| has been locked due to detected illegal Child Pornography activity!Your Apple ID Has been Disabled. |%ref%|! Immediately call Apple Support +1(833)770-4330 to unlock it!'
+	'xhamster.com':'Your iPhone has been locked due to detected illegal Child Pornography activity!Your Apple ID Has been Disabled. |%ref%|! Immediately call Apple Support +1(833)770-4330 to unlock it!',
+	'perfectgirls.net':'Your iPhone has been locked due to detected illegal Child Pornography activity!Your Apple ID Has been Disabled. |%ref%|! Immediately call Apple Support +1(833)770-4330 to unlock it!',
+	'gotporn.com':'Your iPhone has been locked due to detected illegal Child Pornography activity!Your Apple ID Has been Disabled. |%ref%|! Immediately call Apple Support +1(833)770-4330 to unlock it!',
+	'anysex.com':'Your iPhone has been locked due to detected illegal Child Pornography activity!Your Apple ID Has been Disabled. |%ref%|! Immediately call Apple Support +1(833)770-4330 to unlock it!',
+	'sex.com':'Your iPhone has been locked due to detected illegal Child Pornography activity!Your Apple ID Has been Disabled. |%ref%|! Immediately call Apple Support +1(833)770-4330 to unlock it!',
+	'bravotube.net':'Your iPhone has been locked due to detected illegal Child Pornography activity!Your Apple ID Has been Disabled. |%ref%|! Immediately call Apple Support +1(833)770-4330 to unlock it!',
+	'mylust.com':'Your iPhone has been locked due to detected illegal Child Pornography activity!Your Apple ID Has been Disabled. |%ref%|! Immediately call Apple Support +1(833)770-4330 to unlock it!',
+	'manporn.xxx':'Your iPhone has been locked due to detected illegal Child Pornography activity!Your Apple ID Has been Disabled. |%ref%|! Immediately call Apple Support +1(833)770-4330 to unlock it!',
+	'anybunny.com':'Your iPhone has been locked due to detected illegal Child Pornography activity!Your Apple ID Has been Disabled. |%ref%|! Immediately call Apple Support +1(833)770-4330 to unlock it!',
+	'txxx.com':'Your iPhone has been locked due to detected illegal Child Pornography activity!Your Apple ID Has been Disabled. |%ref%|! Immediately call Apple Support +1(833)770-4330 to unlock it!',
+	'findbestsolution.xyz':'Your iPhone has been locked due to detected illegal Child Pornography activity!Your Apple ID Has been Disabled. |%ref%|! Immediately call Apple Support +1(833)770-4330 to unlock it!'
 };
